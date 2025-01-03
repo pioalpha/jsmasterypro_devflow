119197c70d57d3726e43eda1a9ec90e4bf51a7e6
pioalpha <pioalpha@gmail.com>
2025-01-03 14:52:28 +0000

    Create Auth Routes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+
+import Navbar from "@/components/navigation/navbar";
+
+const Rootlayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <Navbar />
+      {children}
+    </main>
+  );
+};
+
+export default Rootlayout;
