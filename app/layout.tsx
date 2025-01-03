cc85da8f5bab69309d637dd0c64610a86fc8a771
pioalpha <pioalpha@gmail.com>
2025-01-03 13:53:37 +0000

    Creating the Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import React from "react";
 
+import Navbar from "@/components/navigation/navbar";
 import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
@@ -47,6 +48,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
