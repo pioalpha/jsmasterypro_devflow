0e9a9797caf3d9f93c288057348b920e38f2c049
pioalpha <pioalpha@gamil.com>
2025-01-18 07:08:02 -0300

    Create Vote Model

---
+++ database/vote.model.ts
@@ -0,0 +1,24 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IVote {
+  author: Types.ObjectId;
+  id: Types.ObjectId;
+  type: string;
+  voteType: string;
+}
+
+const VoteSchema = new Schema<IVote>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    id: { type: Schema.Types.ObjectId },
+    type: { type: String, enum: ["question", "answer"], required: true },
+    voteType: { type: String, enum: ["upvote", "downvote"], required: true },
+  },
+  // Gera o timestamp quando a questão é criada
+  { timestamps: true }
+);
+
+// Verifica se o modelo de questão já está registrado (models), senão cria um novo
+const Vote = models?.Vote || model<IVote>("Vote", VoteSchema);
+
+export default Vote;
