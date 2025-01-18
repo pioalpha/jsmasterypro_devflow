4df30f275ed018a4e98c41a3790e36ad0cc8cb49
pioalpha <pioalpha@gamil.com>
2025-01-18 05:58:15 -0300

    Create Question Model

---
+++ database/question.model.ts
@@ -0,0 +1,33 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+  author: Types.ObjectId;
+  title: string;
+  content: string;
+  tags: Types.ObjectId[];
+  answers: number;
+  upvotes: number;
+  downvotes: number;
+  views: number;
+}
+
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+  },
+  // Gera o timestamp quando a questão é criada
+  { timestamps: true }
+);
+
+// Verifica se o modelo de questão já está registrado (models), senão cria um novo
+const Question =
+  models?.question || model<IQuestion>("Question", QuestionSchema);
+
+export default Question;
---
+++ database/template.model.ts
@@ -0,0 +1,14 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IModel {}
+
+const ModelSchema = new Schema<IModel>(
+  {},
+  // Gera o timestamp quando é criado
+  { timestamps: true }
+);
+
+// Verifica se o modelo já está registrado (models), senão cria um novo
+const Model = models?.model || model<IModel>("Model", ModelSchema);
+
+export default Model;
