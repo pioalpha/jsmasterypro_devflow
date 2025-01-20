c6c8fc8fcc2ad6ffdc0167898a480223bd85563f
pioalpha <pioalpha@gamil.com>
2025-01-20 02:06:34 -0300

    Create and Test Question

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -5,24 +5,31 @@ import mongoose from "mongoose";
 import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
 import TagQuestion from "@/database/tag_question.model";
-import { ActionResponse, ErrorResponse } from "@/types/global";
+import {
+  ActionResponse,
+  ErrorResponse,
+  Question as GlobalQuestion,
+} from "@/types/global";
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
+import logger from "../logger";
 import { AskQuestionSchema } from "../validations";
 
 export async function createQuestion(
   params: CreateQuestionParams
-): Promise<ActionResponse> {
+): Promise<ActionResponse<GlobalQuestion>> {
   const validationResult = await action({
     params,
     schema: AskQuestionSchema,
     authorize: true,
   });
+  logger.info("-4");
 
   if (validationResult instanceof Error) {
     return handleError(validationResult) as ErrorResponse;
   }
+  logger.info("-3");
 
   const { title, content, tags } = validationResult.params!;
   const userId = validationResult?.session?.user?.id;
@@ -31,6 +38,8 @@ export async function createQuestion(
   session.startTransaction();
 
   try {
+    logger.info("-2");
+
     const [newQuestion] = await Question.create(
       [{ title, content, author: userId }],
       { session }
@@ -40,32 +49,36 @@ export async function createQuestion(
       throw new Error("failed to create question");
     }
 
-    const tagsIds: mongoose.Types.ObjectId[] = [];
+    const tagIds: mongoose.Types.ObjectId[] = [];
     const tagQuestionDocuments = [];
 
+    logger.info("-1");
     for (const tag of tags) {
-      const existingTag = await Tag.findByIdAndUpdate(
-        {
-          name: { $regex: new RegExp(`^${tag}$`, "i") },
-        },
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
         { $setOnInsert: { name: tag }, $inc: { questions: 1 } },
-        { upset: true, new: true, session }
+        { upsert: true, new: true, session }
       );
+      logger.info("1");
 
-      tagsIds.push(existingTag._id);
+      tagIds.push(existingTag._id);
+      logger.info("1.5");
       tagQuestionDocuments.push({
         tag: existingTag._id,
         question: newQuestion._id,
       });
+      logger.info("2");
     }
 
     await TagQuestion.insertMany(tagQuestionDocuments, { session });
+    logger.info("3");
 
     await Question.findByIdAndUpdate(
       newQuestion._id,
-      { $push: { tags: { $each: tagsIds } } },
+      { $push: { tags: { $each: tagIds } } },
       { session }
     );
+    logger.info("4");
 
     await session.commitTransaction();
 
