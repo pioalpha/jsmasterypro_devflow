bcbf7c0862648d0902342032b1bca9fdce04b725
pioalpha <pioalpha@gamil.com>
2025-01-20 11:25:57 -0300

    Render UI States

--- constants/routes.ts
+++ constants/routes.ts
@@ -3,10 +3,11 @@ const ROUTES = {
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
   ASK_QUESTION: "/ask-question",
-  EDIT_QUESTION: (id: string) => `/question/${id}/edit`,
+  QUESTION: (id: string) => `/questions/${id}`,
+  EDIT_QUESTION: (id: string) => `${ROUTES.QUESTION(id)}/edit`,
   PROFILE: (id: string) => `/profile/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
-  QUESTION: (id: string) => `/questions/${id}`,
+  COLLECTION: "/collection",
 };
 
 export default ROUTES;
