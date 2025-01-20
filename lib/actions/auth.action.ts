4e66264b806480a12873f582d2fe1ab0cb2073f3
pioalpha <pioalpha@gamil.com>
2025-01-19 21:01:49 -0300

    Sign In Credentials Auth Server Actions

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -9,6 +9,7 @@ import { ActionResponse, ErrorResponse } from "@/types/global";
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
+import { NotFoundError } from "../http-errors";
 import { SignUpSchema } from "../validations";
 
 export async function signUpWithCredentials(
@@ -67,5 +68,42 @@ export async function signUpWithCredentials(
     return handleError(error) as ErrorResponse;
   } finally {
     await session.endSession();
+  }
+}
+
+export async function signInWithCredentials(
+  params: Pick<AuthCredentials, "email" | "password">
+): Promise<ActionResponse> {
+  const validationResult = await action({ params, schema: SignUpSchema });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { email, password } = validationResult.params!;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) throw new NotFoundError("User");
+
+    const existingAccount = await Account.findOne({
+      provider: "credentials",
+      providerAccountId: email,
+    });
+    if (!existingAccount) throw new NotFoundError("Account");
+
+    const passwordMatch = await bcrypt.compare(
+      password,
+      existingAccount.password
+    );
+
+    if (!passwordMatch) throw new Error("Email or Password does not match.");
+
+    await signIn("credentials", { email, password, redirect: false });
+
+    return { success: true };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
   }
 }
